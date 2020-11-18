c2b6faeafa86b1618feca5a58a5b7fea77ccb943
Anthony Chian <anthony@Anthonys-MacBook-Pro.local>
2020-11-17 18:27:25 -0800

    added react compoents for results page

--- routes/api/player.js
+++ routes/api/player.js
@@ -1,74 +1,74 @@
 const { default: Axios } = require("axios")
 const express = require('express')
 const router = express.Router()
+
 var api_key = 'RGAPI-3363731e-06dc-45e7-a524-094b0b91bd26'
-// var name = 'LogicXD'
-// var key = '64'
+//var name = 'LogicXD'
+var key = '64'
+var matchesLimit = 20
 
-test('LogicXD', '64')
+router.get('/:name', async (req, res) => {
+    var name = req.params.name
+    try {
+        let sumURL = 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + name +
+        '?api_key=' + api_key
 
-async function test(name, key) {
-    router.get('/', async (req, res) => {
-        try {
-            let sumURL = 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + name +
-            '?api_key=' + api_key
-
-            let summoner = await new Promise((resolve, reject) => {
-                Axios.get(sumURL).then(res => {
-                    resolve(res.data)
-                })
+        let summoner = await new Promise((resolve, reject) => {
+            Axios.get(sumURL).then(res => {
+                resolve(res.data)
             })
-
-            let matchesURL = 'https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/' + summoner.accountId + '?champion=' + key + '&api_key=' + api_key
-            let matches = await new Promise((resolve, reject) => {
-                Axios.get(matchesURL).then(res => {
-                    resolve(res.data)
-                })
+        })
+        let matchesURL = 'https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/' + summoner.accountId + '?champion=' + key + '&api_key=' + api_key
+        let matches = await new Promise((resolve, reject) => {
+            Axios.get(matchesURL).then(res => {
+                resolve(res.data)
             })
-            let matchesLength = matches.endIndex
-            if (matchesLength > 5) {
-                matchesLength = 5
-            }
-            let gameIds = new Array(matchesLength)
-            for (i = 0; i < matchesLength; i++) {
-                gameIds[i] = matches.matches[i].gameId
-            }
-
-            let matchObjects = []
-
-            for (let i = 0; i < matchesLength; i++) {
-                let gameURL = 'https://na1.api.riotgames.com/lol/match/v4/matches/' + gameIds[i] + '?api_key=' + api_key
-                game = await new Promise((resolve, reject) => {
-                    Axios.get(gameURL).then(res => {
-                        resolve(res.data)
-                    })
-                })
-                let participants = game.participantIdentities
-
-                let currentPlayer = participants.find( ({ player }) => player.summonerName == name )
-                let currentId = currentPlayer.participantId
-                
-                let playerStats = game.participants[currentId].stats
-                var matchObj = {
-                    kills: playerStats.kills,
-                    deaths: playerStats.deaths,
-                    assists: playerStats.assists,
-                    totalDamageDealt: playerStats.totalDamageDealtToChampions,
-                    goldEarned: playerStats.goldEarned,
-                    visionScore: playerStats.visionScore,
-                    win: playerStats.win
-                }
-                matchObjects.push(matchObj)
-            }
-            let avgMatchStats = getAvgMatchStats(matchObjects)
-            res.send(avgMatchStats)
-        } catch (err) {
-            console.error(err.message);
-            return res.status(404).json({ msg: 'error 404' })
+        })
+        let matchesLength = matches.endIndex
+        if (matchesLength > matchesLimit) {
+            matchesLength = matchesLimit
         }
-    })
-    module.exports = router
-}
+        let gameIds = new Array(matchesLength)
+        for (i = 0; i < matchesLength; i++) {
+            gameIds[i] = matches.matches[i].gameId
+        }
+
+        let matchObjects = []
+
+        for (let i = 0; i < matchesLength; i++) {
+            let gameURL = 'https://na1.api.riotgames.com/lol/match/v4/matches/' + gameIds[i] + '?api_key=' + api_key
+            game = await new Promise((resolve, reject) => {
+                Axios.get(gameURL).then(res => {
+                    resolve(res.data)
+                })
+            })
+            let participants = game.participantIdentities
+
+            let currentPlayer = participants.find( ({ player }) => player.summonerName.toLowerCase() == name.toLowerCase() )
+            let currentId = currentPlayer.participantId
+            
+            let playerStats = game.participants[currentId].stats
+            let matchObj = {
+                kills: playerStats.kills,
+                deaths: playerStats.deaths,
+                assists: playerStats.assists,
+                totalDamageDealt: playerStats.totalDamageDealtToChampions,
+                goldEarned: playerStats.goldEarned,
+                visionScore: playerStats.visionScore,
+                win: playerStats.win
+            }
+            matchObjects.push(matchObj)
+        }
+        let avgMatchStats = getAvgMatchStats(matchObjects)
+        console.log(avgMatchStats)
+        res.send(avgMatchStats)
+    } catch (err) {
+        console.error(err.message);
+        return res.status(404).json({ msg: 'error 404' })
+    }
+})
+module.exports = router
+
 
 function getAvgMatchStats(arr) {
     let avgKills = 0
@@ -95,16 +95,17 @@ function getAvgMatchStats(arr) {
     avgDmg = Math.round(avgDmg / arr.length)
     avgGold = Math.round(avgGold / arr.length)
     avgVision = Math.round(avgVision / arr.length)
-    winRate = numWins / arr.length * 100
+    winRate = Math.round(numWins / arr.length * 100)
 
-    var avgStats = {
+    let avgStats = {
+        matches: arr.length,
         kills: avgKills,
         deaths: avgDeaths,
         assists: avgAssists,
         totalDamageDealt: avgDmg,
         goldEarned: avgGold,
         visionScore: avgVision,
-        winRate: winRate + '%'
+        winRate: winRate
     }
     return avgStats
 }
